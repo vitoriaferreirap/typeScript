6ff02f0f5a53a441d33aa2bd4876d9bab7fa08f7
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-16 14:33:23 -0300

    feac: outra maneira de usar função

--- functions.ts
+++ functions.ts
@@ -23,3 +23,22 @@ function performJob(cb: (msg: string) => void) {
     cb('Bom Trabalho'); //passa argumento pois a a função recebe cb como argumento e espera um tipo string
 }
 performJob(log); //passa a função log como argumento, que é do tipo (msg: string) => void
+
+
+//objeto com propriedades e métodos
+type User = {
+    name: string;
+    age: number;
+    greet: () => string; //método greet recebe string e retorna string
+};
+
+let user: User = {
+    name: 'Max',
+    age: 45,
+    greet() {//método greet que satisfaz o tipo definido 
+        console.log('Olá');
+        return this.name;
+    }   
+};
+
+user.greet(); //chama o método greet do objeto user
