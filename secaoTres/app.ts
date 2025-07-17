8f8d7ab424ecb3b554f9afd55b088dd21ab0f83e
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-17 11:54:02 -0300

    feac:criando e configurando tsconfig.json

---
+++ secaoTres/app.ts
@@ -0,0 +1,9 @@
+let userName: string;
+userName = 'John Doe';
+console.log(userName);
+
+
+//fun recebe args de qualquer tipo e retorna qualquer tipo
+function add(x: any, y: any) {
+    return x + y;
+}
