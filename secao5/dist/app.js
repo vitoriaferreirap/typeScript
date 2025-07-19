8083badb1935aa0fa84834f6bb367ca578ce5966
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-19 11:37:50 -0300

    feac:estudando diferenças de var / let e Arrow Function

--- secao5/dist/app.js
+++ secao5/dist/app.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("Hello, World!");
+const userName = 'João';
+let age = 30;
+age = 29;
+const add = (a, b = 1) => a + b;
+const printOutput = output => console.log(output);
+const button = document.querySelector('button');
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+console.log(add(5));
 //# sourceMappingURL=app.js.map
--- secao5/src/app.ts
+++ secao5/src/app.ts
@@ -1,1 +1,42 @@
-console.log("Hello, World!");
+//usando ES6 -
+
+//const vs let
+const userName = 'João';
+let age = 30;
+
+
+//userName = 'Maria'; Erro: não é possível reatribuir uma constante
+age = 29;
+/*
+//var
+function add(a: number, b: number) {
+    var result
+    result = a + b;
+    return result;
+}
+
+var isOlder;
+if (age > 20) {
+    isOlder = true;
+    console.log(isOlder);
+}
+console.log(isOlder); // true, var é global ou local à função
+*/
+
+//sintaxe mais concisa
+//argumento com valor padrão é apenas no ultimo parâmetro
+const add = (a: number, b: number = 1) => a + b;
+
+//isso é uma função anônima - Arrow Function
+//significa que não tem nome, é uma função de callback
+//função de callback é uma função que é passada como argumento para outra função
+//e é executada dentro dessa função
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+console.log(add(5));
