6c91f712dbc4335458d3193e6f24085217f62324
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-23 14:37:50 -0300

    feac: acessando elementos, e add config e evento de submit

--- typeScript2/src/app.ts
+++ typeScript2/src/app.ts
@@ -6,10 +6,13 @@
 //acessar formulario.html
 class ProjectInput{
     //Template é um elemento HTML que serve como modelo para criar novos elementos.
-    //acessar o template e o host
+    //acessar o template e o (host - local onde o template será inserido)
     templateElement: HTMLTemplateElement; 
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
     //acessar o elemento do template
     constructor() {
@@ -20,12 +23,33 @@ class ProjectInput{
         //content : acessa o conteúdo do template. 
         const importeNode = document.importNode(this.templateElement.content, true);
         this.element = importeNode.firstElementChild as HTMLFormElement; // pega o primeiro elemento filho do template
+        //adicionar um id ao elemento
+        this.element.id = 'user-input';
+
+        
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
         this.attach();
     }
 
-        private attach() {
-            this.hostElement.insertAdjacentElement('afterbegin', this.element);
-        }
+    //método para anexar o elemento ao template
+
+    //método para configurar o evento de submit
+    private submitHandler(event: Event) {
+        event.preventDefault(); //previne o comportamento padrão do formulário de recarregar a página
+        console.log(this.titleInputElement.value);
+        console.log(this.descriptionInputElement.value);
+        console.log(this.peopleInputElement.value);
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));//
+    }
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
 
 }
 
