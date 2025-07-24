6362fbb90c1b72c09a512e6a19bfdb87de91cb83
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-24 11:35:17 -0300

    feac:aplicando herança

--- typeScript2/src/app.ts
+++ typeScript2/src/app.ts
@@ -4,7 +4,6 @@ enum ProjectStatus {
     Finished
 }
 
-
 class Project {
     constructor(
         public id: string,
@@ -18,14 +17,24 @@ class Project {
 //Project state management
 
 //listener personalizado
-type Listener = (items: Project[]) => void;
+type Listener<T> = (items: T[]) => void;
 
-class ProjectState {
-    private listeners:Listener[] = []; //array para armazenar os ouvintes que serão notificados quando um novo projeto for adicionado
+class State<T> {
+    //protected - acesse classes que herdam State
+    protected listeners: Listener<T>[] = []; //array para armazenar os ouvintes
+
+     addListener(listenerFn: Listener<T>) {
+        this.listeners.push(listenerFn); //adiciona o ouvinte ao array de ouvintes
+    }
+}
+
+class ProjectState  extends State<Project> {
     private projects: Project[] = []; //array para armazenar os projetos
     private static instance: ProjectState; //variável para armazenar a instância única da classe
 
-    private constructor() {}
+    private constructor() {
+        super();
+    }
 
     //método para obter a instância única da classe
     static getInstance() {
@@ -34,10 +43,6 @@ class ProjectState {
         }
         this.instance = new ProjectState(); //cria uma nova instância se não existir (nova lista de projetos)
         return this.instance;
-    }
-
-    addListener(listenerFn: Listener) {
-        this.listeners.push(listenerFn); //adiciona o ouvinte ao array de ouvintes
     }
 
     //projeto inicia ativo
@@ -70,8 +75,6 @@ class ProjectState {
 
 //instancia um obj de estado que sera usado para gerenciar os projetos
 const projectState = ProjectState.getInstance();
-
-
 
 //Validation usando interfaces
 //A interface Validatable define as regras de validação que podem ser aplicadas a diferentes tipos de dados.
@@ -107,9 +110,6 @@ function validate(validatableInput: Validatable) {
     return isValid; //retorna o resultado da validação
 }
 
-
-
-
 //autobind decorator
 /**
  * Decorator criado para vincular o contexto de 'this' ao método,
@@ -133,27 +133,57 @@ function autobind(
     return adjustedDescriptor;
 }
 
+//Component Base Class - define estrutura básica para componentes
+//class abstrata - base para herança para outros componentes
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(templateId: string, hostElementId: string, insertAtStart: boolean, newElementId?: string) {
+        this.templateElement = document.getElementById(templateId)! as HTMLTemplateElement;
+        this.hostElement = document.getElementById(hostElementId)! as T;// T é o tipo do elemento host (HTMLDivElement)
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as U;
+
+        //aplicando operação ternária
+        this.element.id = newElementId ? newElementId : ''; //se newElementId for fornecido, define o id do elemento, caso contrário, deixa vazio
+    
+        this.attach(insertAtStart); //anexa o elemento ao hostElement
 
 
+    }   
+    //método para anexar o elemento ao hostElement
+    private attach(insertAtBeginning: boolean) {
+        this.hostElement.insertAdjacentElement(insertAtBeginning ? 'afterbegin' : 'beforeend', this.element); //anexa o elemento ao hostElement
+    }
+  
+    //métodos abstratos são definidos na classe base e devem ser implementados nas classes filhas
+    abstract configure(): void;
+    abstract renderContent(): void; 
+}
 
 //ProjectList Class
-class ProjectList {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    element: HTMLElement;
+class ProjectList extends Component<HTMLDivElement, HTMLElement> {
+
     assignedProjects: Project[]; //array para armazenar os projetos atribuídos à lista (active ou finished)
 
     constructor(private type: 'active' | 'finished') {
-        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
+        //super = chama o construtor da classe base Component
+        super('project-list', 'app', false, `${type}-projects`); 
         this.assignedProjects = []; //inicializa o array de projetos atribuídos
+        
+        this.configure(); //chama o método de configuração para adicionar ouvintes e renderizar os projetos
+       
+        this.renderContent(); 
+        
+    }
 
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLElement;
-        this.element.id = `${this.type}-projects`; //define o id do elemento com base no tipo (active ou finished)
-
-        //adiciona um ouvinte ao estado do projeto para atualizar a lista quando um novo projeto for adicionado
+       //método para configurar o componente (Usado para resolver problemas de contexto)
+    configure() {
+         //ADD OUVINTE ao estado do projeto para atualizar a lista quando um novo projeto for adicionado
         projectState.addListener((projects: Project[]) => {
             const relevantProjects = projects.filter(prj => {
                 if(this.type === 'active') {
@@ -164,10 +194,14 @@ class ProjectList {
             this.assignedProjects = relevantProjects;
             this.renderProjects(); //chama o método para renderizar os projetos atribuídos
         });
-
-        this.attach();
-        this.renderContent(); 
         
+     }
+    //método para renderizar o conteúdo do template de lista de projetos
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId; //define o id da lista dentro do elemento
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase()
+        + ' PROJECTS'; //define o texto do título com base no tipo (active ou finished)
     }
 
     //método para renderizar a lista de projetos atribuídos
@@ -180,55 +214,36 @@ class ProjectList {
             listEl.appendChild(listItem); //adiciona o item de lista à lista
         }
     }
-
-    //método para renderizar o conteúdo do template de lista de projetos
-    private renderContent() {
-        const listId = `${this.type}-projects-list`;
-        this.element.querySelector('ul')!.id = listId; //define o id da lista dentro do elemento
-        this.element.querySelector('h2')!.textContent = this.type.toUpperCase()
-        + ' PROJECTS'; //define o texto do título com base no tipo (active ou finished)
-    }
-
-    private attach() {
-        this.hostElement.insertAdjacentElement('beforeend', this.element);
-    }
 }
 
-
 //ProjectInput Class
-class ProjectInput{
+class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     /**
  * O código TypeScript (app.ts), depois de compilado para JavaScript (app.js),
  * é responsável por pegar o conteúdo do template e inserir na página. TONANDO VISIVEL
  * Template é um elemento HTML que serve como modelo para criar novos elementos.
  * Acessar o template e o (host - local onde o template será inserido)
  */
-    templateElement: HTMLTemplateElement; 
-    hostElement: HTMLDivElement;
-    element: HTMLFormElement;
+  
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
 
     //acessar o elemento do template
     constructor() {
-        // ! as HTMLTemplateElement é uma asserção de tipo que diz ao TypeScript que o elemento não será nulo
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+        super('project-input', 'app', true, 'user-input'); //chama o construtor da classe base Component
 
-        //content : acessa o conteúdo do template. 
-        const importeNode = document.importNode(this.templateElement.content, true);
-        this.element = importeNode.firstElementChild as HTMLFormElement; // pega o primeiro elemento filho do template
-        //adicionar um id ao elemento
-        this.element.id = 'user-input';
-
-        
         this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
         this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
         this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
-
+     
         this.configure();
-        this.attach();
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler);
+    }
+    renderContent() {
+        //não é necessário implementar este método, pois não há conteúdo a ser renderizado no template de entrada
     }
 
     //métodos para anexar e manipular elementos ao template
@@ -272,6 +287,12 @@ class ProjectInput{
 
     }
 
+     private clearInputs() {
+        this.titleInputElement.value = ''; //limpa o campo de título
+        this.descriptionInputElement.value = ''; 
+        this.peopleInputElement.value = ''; //limpa o campo de número de pessoas
+    }
+
     //método para configurar o evento de submit
     @autobind //decorator para vincular o contexto de 'this' ao método
     private submitHandler(event: Event) {
@@ -282,16 +303,10 @@ class ProjectInput{
         if (Array.isArray(userInput)) {
             const [title, description, people] = userInput;
             projectState.addProject(title, description, people); //adiciona o projeto ao estado global
-            //this.clearInputs(); //limpa os campos de entrada após a validação bem-sucedida
+            this.clearInputs(); //limpa os campos de entrada após a validação bem-sucedida
         }
     }
-    private configure() {
-        this.element.addEventListener('submit', this.submitHandler);
-    }
-    private attach() {
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
-    }
-
+    
 }
 
 //instanciando um novo objeto em cima da classe
