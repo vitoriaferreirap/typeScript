1832544a961e94cd233d14eed0511bba2b593dcf
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-25 11:35:28 -0300

    feat:criando projeto que consumira API do Google, aplicando bibliotecas como HDP e Google maps

--- typeScript2/exercicioPratico2/src/app.ts
+++ typeScript2/exercicioPratico2/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+//acessando forms
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement; // não sabe que tipo de elemento é
+
+function searchAddress(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    //indo para a API do google
+}
+
+form.addEventListener('submit', searchAddress);
